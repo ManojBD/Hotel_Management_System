200104a463cbfbb5d0b01d585a07c1a5fc5b569f
chathuranuwan <chathuranuwan996@gmail.com>
2022-02-07 20:22:30 +0530

    dashboard

--- hotel_management_stm_front_end/src/App.js
+++ hotel_management_stm_front_end/src/App.js
@@ -4,7 +4,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Buttonpage from "./components/Buttonpage";
-import AdminSignin from "./components/SigninPages/adminSignin";
+import Signin from "./components/SigninPages/Signin";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="loginpage" exact element={<Buttonpage />} />
-          <Route path="adminsignin" exact element={<AdminSignin />} />
+          <Route path="Signin" exact element={<Signin />} />
+          <Route path="dashboard" exact element={<Dashboard />} />
         </Routes>
       </Router>
     </>
--- hotel_management_stm_front_end/src/components/Navbar.js
+++ hotel_management_stm_front_end/src/components/Navbar.js
@@ -37,7 +37,7 @@ function Navbar() {
             </li>
             <li className="nav-item">
               <Link
-                to="loginpage"
+                to="/signin"
                 className="nav-links"
                 onClick={closeMobileMenu}
               >
