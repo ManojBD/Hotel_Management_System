27d86c18c6a09ececa07640cac01bef1e582a34e
chathuranuwan <chathuranuwan996@gmail.com>
2022-03-27 15:46:08 +0530

    Route Signup page

--- hotel_management_stm_front_end/src/App.js
+++ hotel_management_stm_front_end/src/App.js
@@ -6,16 +6,17 @@ import Home from "./components/pages/Home";
 import Buttonpage from "./components/Buttonpage";
 import Signin from "./components/SigninPages/Signin";
 import Dashboard from "./components/dashboard/Dashboard";
+import Signup from "./components/SigninPages/Signup";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="loginpage" exact element={<Buttonpage />} />
           <Route path="Signin" exact element={<Signin />} />
+          <Route path="Signup" exact element={<Signup />} />
           <Route path="dashboard" exact element={<Dashboard />} />
         </Routes>
       </Router>
